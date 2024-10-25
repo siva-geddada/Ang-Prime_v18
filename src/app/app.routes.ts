c92a12bfff19475f56b38fefcb3416995bfb08e1
Siva_Geddada <siva.cs579@gmail.com>
2024-10-25 16:54:44 +0530

    added new component changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { GetStartedComponent } from './features/get-started/get-started.component';
+import { FormsComponent } from './features/forms/forms/forms.component';
+import { FormArraysComponent } from './features/forms/form-arrays/form-arrays.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'get-started', pathMatch: 'full' },
+    { path: 'get-started', component: GetStartedComponent },
+    { path: 'forms', component: FormsComponent },
+    { path: 'form-array', component: FormArraysComponent },
+    { path: '**', redirectTo: 'home' },
+  
+];
---
+++ src/app/features/forms/form-arrays/form-arrays.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-arrays',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-arrays.component.html',
+  styleUrl: './form-arrays.component.scss'
+})
+export class FormArraysComponent {
+
+}
