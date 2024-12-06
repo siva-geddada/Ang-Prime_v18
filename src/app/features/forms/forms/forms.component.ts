1e7a85448f1f23e5c0712ab2a46c0a5023da34e7
Siva_Geddada <siva.cs579@gmail.com>
2024-12-06 23:25:22 +0530

    changes added

--- src/app/features/forms/forms/forms.component.ts
+++ src/app/features/forms/forms/forms.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { InputTextModule } from 'primeng/inputtext';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [DatePickerModule, SelectModule, InputTextModule, FormsModule, InputTextareaModule, CommonModule],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
+  countries: any[] | undefined;
 
+  selectedCountry: any;
+
+  ngOnInit(): void {
+    this.countries = [
+      { name: 'Australia', code: 'AU' },
+      { name: 'Brazil', code: 'BR' },
+      { name: 'China', code: 'CN' },
+      { name: 'Egypt', code: 'EG' },
+      { name: 'France', code: 'FR' },
+      { name: 'Germany', code: 'DE' },
+      { name: 'India', code: 'IN' },
+      { name: 'Japan', code: 'JP' },
+      { name: 'Spain', code: 'ES' },
+      { name: 'United States', code: 'US' },
+    ];
+  }
 }
